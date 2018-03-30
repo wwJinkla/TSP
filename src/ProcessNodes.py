4db17245b2376adcf48083603b8a1ebc857b121c
Gabriel Head <gabrielhead@Gabriels-MacBook-Air.local>
2018-03-30 15:58:37 -0500

    Committing preprocessing script for nodes

---
+++ src/ProcessNodes.py
@@ -0,0 +1,20 @@
+import scipy.io
+import pandas as pd
+from itertools import *
+import numpy as np
+import os
+from sklearn import linear_model
+
+script_dir = os.path.dirname(__file__)
+data_dir = os.path.join(script_dir, '../../TSP')
+
+print data_dir
+
+
+os.chdir(data_dir)
+
+
+for (dirpath, dirnames, filenames) in os.walk(os.getcwd()):
+    for f in filenames:
+        if f[-3:] == "txt":
+            print "Reading file: " + f
